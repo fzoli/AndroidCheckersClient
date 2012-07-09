201113b53f0febd400f9b10f0dda329c41de6459
fzoli <f.zoli@mailbox.hu>
2012-07-09 15:27:30 +0200

    3

--- AndroidMillClient/src/org/dyndns/fzoli/mill/common/key/PlayerAvatarKeys.java
+++ AndroidMillClient/src/org/dyndns/fzoli/mill/common/key/PlayerAvatarKeys.java
@@ -9,9 +9,11 @@ public interface PlayerAvatarKeys {
     String KEY_X = "x";
     String KEY_Y = "y";
     String KEY_SCALE = "scale";
+    String KEY_USER = "user";
     
     String REQ_SET_AVATAR_ATTRS = "set_avatar_attrs";
     String REQ_GET_AVATAR = "get_avatar";
     String REQ_SET_AVATAR = "set_avatar";
+    String REQ_REMOVE_AVATAR = "remove_avatar";
     
 }
