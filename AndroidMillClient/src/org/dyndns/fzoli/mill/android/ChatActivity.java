d447b95a3f8326cf5668586b3e0c9188dc46b1c7
fzoli <f.zoli@mailbox.hu>
2012-07-23 15:10:51 +0200

    3

--- AndroidMillClient/src/org/dyndns/fzoli/mill/android/ChatActivity.java
+++ AndroidMillClient/src/org/dyndns/fzoli/mill/android/ChatActivity.java
@@ -4,8 +4,10 @@ import java.text.DateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 import java.util.Map.Entry;
 
 import org.dyndns.fzoli.mill.android.activity.AbstractMillOnlineActivity;
@@ -22,7 +24,9 @@ import org.dyndns.fzoli.mvc.client.connection.Connection;
 import org.dyndns.fzoli.mvc.client.event.ModelActionEvent;
 import org.dyndns.fzoli.mvc.client.event.ModelActionListener;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.text.Spannable;
 import android.text.SpannableStringBuilder;
@@ -34,25 +38,26 @@ import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.ScrollView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
 public class ChatActivity extends AbstractMillOnlineActivity<ChatEvent, ChatData> {
 
 	public static final String KEY_PLAYER = "player";
 	
-	private static final HashMap<String, Integer> emoticons = new HashMap<String, Integer>() {
+	private static final Map<String, Integer> emoticons = new HashMap<String, Integer>() {
 		
 		private static final long serialVersionUID = 1L;
 
 		{
-			put(":)", R.drawable.emo_im_happy);
 			put(":-)", R.drawable.emo_im_happy);
-			put(":(", R.drawable.emo_im_sad);
+			put(":)", R.drawable.emo_im_happy);
 			put(":-(", R.drawable.emo_im_sad);
-			put(";)", R.drawable.emo_im_winking);
+			put(":(", R.drawable.emo_im_sad);
 			put(";-)", R.drawable.emo_im_winking);
-			put(":P", R.drawable.emo_im_tongue_sticking_out);
+			put(";)", R.drawable.emo_im_winking);
 			put(":-P", R.drawable.emo_im_tongue_sticking_out);
+			put(":P", R.drawable.emo_im_tongue_sticking_out);
 			put("=-O", R.drawable.emo_im_surprised);
 			put(":-*", R.drawable.emo_im_kissing);
 			put(":O", R.drawable.emo_im_yelling);
@@ -65,13 +70,47 @@ public class ChatActivity extends AbstractMillOnlineActivity<ChatEvent, ChatData
 			put(":'(", R.drawable.emo_im_crying);
 			put(":X", R.drawable.emo_im_lips_are_sealed);
 			put(":-X", R.drawable.emo_im_lips_are_sealed);
-			put(":D", R.drawable.emo_im_laughing);
 			put(":-D", R.drawable.emo_im_laughing);
+			put(":D", R.drawable.emo_im_laughing);
 			put("o_O", R.drawable.emo_im_wtf);
 		}
 		
 	};
 	
+	private static final List<Integer> emoticonList = new ArrayList<Integer>();
+	
+	private static final Map<Integer, Integer> emoticonTexts = new HashMap<Integer, Integer>() {
+		
+		private static final long serialVersionUID = 1L;
+
+		{
+			emoticonList.clear();
+			put(R.drawable.emo_im_happy, R.string.emo_im_happy);
+			put(R.drawable.emo_im_sad, R.string.emo_im_sad);
+			put(R.drawable.emo_im_winking, R.string.emo_im_winking);
+			put(R.drawable.emo_im_tongue_sticking_out, R.string.emo_im_tongue_sticking_out);
+			put(R.drawable.emo_im_surprised, R.string.emo_im_surprised);
+			put(R.drawable.emo_im_kissing, R.string.emo_im_kissing);
+			put(R.drawable.emo_im_yelling, R.string.emo_im_yelling);
+			put(R.drawable.emo_im_cool, R.string.emo_im_cool);
+			put(R.drawable.emo_im_money_mouth, R.string.emo_im_money_mouth);
+			put(R.drawable.emo_im_foot_in_mouth, R.string.emo_im_foot_in_mouth);
+			put(R.drawable.emo_im_embarrassed, R.string.emo_im_embarrassed);
+			put(R.drawable.emo_im_angel, R.string.emo_im_angel);
+			put(R.drawable.emo_im_undecided, R.string.emo_im_undecided);
+			put(R.drawable.emo_im_crying, R.string.emo_im_crying);
+			put(R.drawable.emo_im_lips_are_sealed, R.string.emo_im_lips_are_sealed);
+			put(R.drawable.emo_im_laughing, R.string.emo_im_laughing);
+			put(R.drawable.emo_im_wtf, R.string.emo_im_wtf);
+		}
+		
+		public Integer put(Integer key, Integer value) {
+			emoticonList.add(key);
+			return super.put(key, value);
+		};
+		
+	};
+
 	private String sender, address;
 	private List<Message> messages;
 	
@@ -123,6 +162,15 @@ public class ChatActivity extends AbstractMillOnlineActivity<ChatEvent, ChatData
 			
 		});
 		
+		etChat.setOnLongClickListener(new View.OnLongClickListener() {
+			
+			@Override
+			public boolean onLongClick(View paramView) {
+				showSmileyList();
+				return true;
+			}
+			
+		});
 	}
 	
 	@Override
@@ -133,6 +181,53 @@ public class ChatActivity extends AbstractMillOnlineActivity<ChatEvent, ChatData
 	@Override
 	public ChatModel getModel() {
 		return (ChatModel) super.getModel();
+	}
+	
+	private void showSmileyList() {
+		final String KEY_ICON = "icon", KEY_LABEL = "label", KEY_TEXT = "text";
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(getString(R.string.insert_smiley));
+       
+        final List<Map<String,Object>> objRows = new ArrayList<Map<String,Object>>();
+        for (Integer key : emoticonList) {
+        	String text = "";
+        	Iterator<Entry<String, Integer>> it = emoticons.entrySet().iterator();
+        	while (it.hasNext()) {
+        		Entry<String, Integer> e2 = it.next();
+        		if (e2.getValue().equals(key)) {
+        			text = e2.getKey();
+        			break;
+        		}
+        	}
+        	
+        	Map<String, Object> objRow = new HashMap<String, Object>();
+            objRow.put(KEY_ICON, key);
+            objRow.put(KEY_LABEL, getString(emoticonTexts.get(key)));
+            objRow.put(KEY_TEXT, text);
+        	objRows.add(objRow);
+        }
+        
+        String[] strKeys = new String[] {KEY_ICON, KEY_LABEL, KEY_TEXT};
+        int[] intViewIds = new int[] {R.id.ivEmoIcon, R.id.tvEmoLabel, R.id.tvEmoText};
+        SimpleAdapter objEmoticonList = new SimpleAdapter(this, objRows, R.layout.emoticons, strKeys, intViewIds);
+        builder.setAdapter(objEmoticonList , new DialogInterface.OnClickListener() {
+			
+			@Override
+			public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+				String text = objRows.get(paramInt).get(KEY_TEXT).toString();
+				int position = etChat.getSelectionStart();
+				String msg = etChat.getText().toString();
+				String start = msg.substring(0, position);
+				start += text;
+				String end = msg.substring(position);
+				msg = start + end;
+				etChat.setText(msg);
+				etChat.setSelection(position + text.length());
+			}
+			
+		});
+        
+        builder.show();
 	}
 	
 	private Spannable getSmiledText(String text) {
